f0282d6a0f59d0dec3cac87676118aa229c79a61
Martin Fencl <martin.fencl@gmail.com>
2019-01-29 21:45:23 -0600

    generated controller services, the views, and update the nav bar

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,43 @@
+class ServicesController < ApplicationController
+  def kitchens
+  end
+
+  def bathrooms
+  end
+
+  def roofing
+  end
+
+  def green
+  end
+
+  def siding
+  end
+
+  def windows
+  end
+
+  def decks
+  end
+
+  def gutters
+  end
+
+  def flooring
+  end
+
+  def stone
+  end
+
+  def hail
+  end
+
+  def ice
+  end
+
+  def snow
+  end
+
+  def bats
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,20 @@ Rails.application.routes.draw do
   get '/about',   to: 'pages#about'
   get '/contact', to: 'pages#contact'
 
+  get '/kitchens',  to: 'services#kitchens'
+  get '/bathrooms', to: 'services#bathrooms'
+  get '/roofing',   to: 'services#roofing'
+  get '/green',     to: 'services#green'
+  get '/siding',    to: 'services#siding'
+  get '/windows',   to: 'services#windows'
+  get '/decks',     to: 'services#decks'
+  get '/gutters',   to: 'services#gutters'
+  get '/flooring',  to: 'services#flooring'
+  get '/stone',     to: 'services#stone'
+  get '/hail',      to: 'services#hail'
+  get '/ice',       to: 'services#ice'
+  get '/snow',      to: 'services#snow'
+  get '/bats',      to: 'services#bats'
+
   get '/testimonials', to: 'testimonials#index'
 end
