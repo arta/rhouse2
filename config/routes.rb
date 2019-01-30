57871445d1828e48b8bca2a1000454ad8c4f869a
Martin Fencl <martin.fencl@gmail.com>
2019-01-29 19:23:54 -0600

    generate testimonials controller, structure and style the index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/about',   to: 'pages#about'
   get '/contact', to: 'pages#contact'
 
+  get '/testimonials', to: 'testimonials#index'
+
   root 'pages#home'
 end
