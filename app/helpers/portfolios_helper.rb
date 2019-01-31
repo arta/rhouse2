68620240ecbfe4d8f595d2c3fcaced62a1e1cfd3
Martin Fencl <martin.fencl@gmail.com>
2019-01-31 12:56:26 -0600

    refactor services and portfolios nav helpers
    Enhance code readability.

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,4 +1,8 @@
 module PortfoliosHelper
+  def current_portfolio?
+    controller_name == 'portfolios' && action_name.in?(portfolios_actions)
+  end
+
   def portfolios_actions
     %w(index flooring windows siding roofing decks)
   end
--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,10 +1,22 @@
 module ServicesHelper
+  def current_service_exterior?
+    controller_name == 'services' && action_name.in?(exterior_actions)
+  end
+
   def exterior_actions
     %w(exterior siding roofing green gutters decks)
   end
 
+  def current_service_interior?
+    controller_name == 'services' && action_name.in?(interior_actions)
+  end
+
   def interior_actions
     %w(interior kitchens bathrooms flooring stone windows)
+  end
+
+  def current_service_mend?
+    controller_name == 'services' && action_name.in?(mend_actions)
   end
 
   def mend_actions
