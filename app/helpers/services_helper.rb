d892d926fe0ece68ec41c91da536c4726c388bf8
Martin Fencl <martin.fencl@gmail.com>
2019-01-30 23:55:44 -0600

    refactor services nav with a helper

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,13 @@
 module ServicesHelper
+  def exteriors
+    %w(siding roofing green gutters decks)
+  end
+
+  def interiors
+    %w(kitchens bathrooms flooring stone windows)
+  end
+
+  def repairs
+    %w(hail ice snow bats)
+  end
 end
