1aa30d629add17197f54d91c7f137096f3a845ec
Martin Fencl <martin.fencl@gmail.com>
2019-02-25 12:31:07 -0600

    tidy up scaffolded portfolio admin code, remove redundant files
    Remove json views.

--- app/controllers/admin/portfolios_controller.rb
+++ app/controllers/admin/portfolios_controller.rb
@@ -2,13 +2,11 @@ class Admin::PortfoliosController < ApplicationController
   before_action :set_admin_portfolio, only: [:show, :edit, :update, :destroy]
 
   # GET /admin/portfolios
-  # GET /admin/portfolios.json
   def index
     @admin_portfolios = Admin::Portfolio.all
   end
 
   # GET /admin/portfolios/1
-  # GET /admin/portfolios/1.json
   def show
   end
 
@@ -22,43 +20,29 @@ class Admin::PortfoliosController < ApplicationController
   end
 
   # POST /admin/portfolios
-  # POST /admin/portfolios.json
   def create
     @admin_portfolio = Admin::Portfolio.new(admin_portfolio_params)
 
-    respond_to do |format|
-      if @admin_portfolio.save
-        format.html { redirect_to @admin_portfolio, notice: 'Portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @admin_portfolio }
-      else
-        format.html { render :new }
-        format.json { render json: @admin_portfolio.errors, status: :unprocessable_entity }
-      end
+    if @admin_portfolio.save
+      redirect_to @admin_portfolio, notice: 'Portfolio was created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /admin/portfolios/1
-  # PATCH/PUT /admin/portfolios/1.json
   def update
-    respond_to do |format|
-      if @admin_portfolio.update(admin_portfolio_params)
-        format.html { redirect_to @admin_portfolio, notice: 'Portfolio was successfully updated.' }
-        format.json { render :show, status: :ok, location: @admin_portfolio }
-      else
-        format.html { render :edit }
-        format.json { render json: @admin_portfolio.errors, status: :unprocessable_entity }
-      end
+    if @admin_portfolio.update(admin_portfolio_params)
+      redirect_to @admin_portfolio, notice: 'Portfolio was updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /admin/portfolios/1
-  # DELETE /admin/portfolios/1.json
   def destroy
     @admin_portfolio.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_portfolios_url, notice: 'Portfolio was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to admin_portfolios_url, notice: 'Portfolio was destroyed.'
   end
 
   private
@@ -69,6 +53,7 @@ class Admin::PortfoliosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admin_portfolio_params
-      params.require(:admin_portfolio).permit(:service, :description, :job_started_at, :job_completed_at)
+      params.require(:admin_portfolio).permit(:service, :description,
+        :job_started_at, :job_completed_at)
     end
 end
--- app/views/admin/portfolios/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @admin_portfolios, partial: 'admin_portfolios/admin_portfolio', as: :admin_portfolio
