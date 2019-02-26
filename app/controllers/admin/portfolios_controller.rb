c4edb11f9977ccdbd01c5fb8f6da7f7ef352ea9c
Martin Fencl <martin.fencl@gmail.com>
2019-02-26 16:27:18 -0600

    fix unpermitted parameters
    Remove, and avoid calling .permit!  Be safe.

--- app/controllers/admin/portfolios_controller.rb
+++ app/controllers/admin/portfolios_controller.rb
@@ -53,8 +53,8 @@ class Admin::PortfoliosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admin_portfolio_params
-      params.require(:admin_portfolio).permit!
-      # params.require(:admin_portfolio).permit(:service, :description,
-      #   :job_started_at, :job_completed_at, images_attributes: [])
+      params.require(:admin_portfolio).permit(:service, :description,
+        :job_started_at, :job_completed_at,
+        images_attributes: [:id, :comment, :file])
     end
 end
