56a64f75e335de295370deee97c587ed5eaafd3d
Martin Fencl <martin.fencl@gmail.com>
2019-02-26 16:30:59 -0600

    destroy redundant admin attachments controller
    Again, it was supplanted by admin images controller in order to handle adding comments to images (via a join model, table Image, images).

--- app/controllers/admin/attachments_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::AttachmentsController < ApplicationController
-  # DELETE /admin/attachments/1
-  def destroy
-    image = ActiveStorage::Attachment.find(params[:id])
-    portfolio = image.record
-    image.purge
-    redirect_to portfolio
-  end
-end
