f0c7df942d108d983d8ef5800eec1d0a2cdd682d
Martin Fencl <martin.fencl@gmail.com>
2019-02-27 17:44:44 -0600

    implement bare bones devise authentication
    Works.
    
    TODO: 1) Setup basic http authenticaiton for new user registration pages, 2) Style user links, show them only on admin pages.

--- app/controllers/admin/portfolios_controller.rb
+++ app/controllers/admin/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class Admin::PortfoliosController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_admin_portfolio, only: [:show, :edit, :update, :destroy]
 
   # GET /admin/portfolios
--- app/controllers/admin/testimonials_controller.rb
+++ app/controllers/admin/testimonials_controller.rb
@@ -1,4 +1,5 @@
 class Admin::TestimonialsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_admin_testimonial, only: [:show, :edit, :update, :destroy]
 
   # GET /admin/testimonials
