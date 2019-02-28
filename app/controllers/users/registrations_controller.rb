fed0e67f54139737cdda7c6d9556f79aff446f06
Martin Fencl <martin.fencl@gmail.com>
2019-02-27 22:28:30 -0600

    restrict access to new user registration w basic http auth
    Dead simple.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  http_basic_authenticate_with name: 'rhouse2', password: 'welcome', only: :new
+
   # GET /resource/sign_up
   def new
     super
