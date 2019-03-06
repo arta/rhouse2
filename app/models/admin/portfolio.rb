214b3150003bc36b6d7174f1ebc9bb19c3e80f07
Martin Fencl <martin.fencl@gmail.com>
2019-03-05 19:18:00 -0600

    offere portfolio's image show order from a "smart" select
    Options for select are derived from unavailable image show order (already taken).

--- app/models/admin/portfolio.rb
+++ app/models/admin/portfolio.rb
@@ -12,7 +12,15 @@ class Admin::Portfolio < ApplicationRecord
   scope :with_attached_images,
     -> { includes(:images).merge(Image.with_attached_file) }
 
-  def image_show_order_taken
-    images.pluck(:show_order).join(', ')
+  def unavailable_image_show_order
+    images.pluck(:show_order).compact
+  end
+
+  def available_image_show_order
+    case unavailable_image_show_order.size
+    when 0 then [1]
+    else
+      (1..unavailable_image_show_order.size+2).to_a-unavailable_image_show_order
+    end
   end
 end
