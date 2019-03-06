bf0b69c56b739ddea6e04c58ff1bf59290329714
Martin Fencl <martin.fencl@gmail.com>
2019-03-05 21:12:38 -0600

    validate presence of image show_order

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,6 +6,7 @@ class Image < ApplicationRecord
   validate :file_is_attached?
   validate :file_is_image?
 
+  validates :show_order, presence: true
   validates_numericality_of :show_order, only_integer: true, greater_than: 0
   validates_uniqueness_of :show_order, scope: :portfolio_id
 
