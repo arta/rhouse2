a0d0ac13ca8c14db871b454011c595ce8e7b3c4f
Martin Fencl <martin.fencl@gmail.com>
2019-03-05 20:31:52 -0600

    add custom image file attached validation
    Prevent crashes due to images created w/o files attached.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,8 +3,15 @@ class Image < ApplicationRecord
   has_one_attached :file
   delegate_missing_to :file
 
+  validate :file_attached?
+
   validates_numericality_of :show_order, only_integer: true, greater_than: 0
   validates_uniqueness_of :show_order, scope: :portfolio_id
 
   scope :ordered, -> { order(show_order: :asc) }
+
+  private
+    def file_attached?
+      errors.add(:file, :missing) unless file.attached?
+    end
 end
