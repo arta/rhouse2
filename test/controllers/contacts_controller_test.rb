4a145a30813106263bfd875dd53e141bbde60590
Martin Fencl <martin.fencl@gmail.com>
2019-03-17 21:37:33 -0500

    (re)write failing test of contacts#new

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get contacts_new_url
-    assert_response :success
-  end
+  test "should GET new" do
+    get new_contact_url
 
+    assert_response :success
+
+    assert_select 'form' do
+      assert_select 'input[type=text]'
+      assert_select 'input[type=email]'
+      assert_select 'textarea'
+      assert_select 'input[type=submit]'
+    end
+  end
 end
