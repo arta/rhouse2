cfd88ca6fae123650c9982e82182781c4c869d2b
Martin Fencl <martin.fencl@gmail.com>
2019-03-17 23:25:41 -0500

    make contacts#create test pass without validating Inquiry
    Simply make contacts#create redirect to #new.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,8 @@ class ContactsController < ApplicationController
   def new
     @inquiry = Inquiry.new
   end
+
+  def create
+    redirect_to new_inquiry_path, notice: 'Inquiry received, thanks!'
+  end
 end
