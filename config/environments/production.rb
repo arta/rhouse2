602962441b9868e621d8a15d533d87247dad8c25
Martin Fencl <martin.fencl@gmail.com>
2019-03-31 16:26:19 -0500

    set up sendgrid on staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,17 @@ Rails.application.configure do
 
   # 2-27-19, devise:
   config.action_mailer.default_url_options = { host: 'rhouse2-staging.herokuapp.com' }
+
+  # 3-31-19, sendgrid:
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'rhouse2-staging.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
