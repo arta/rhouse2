f836c954fb3e839ab6c8521e5d4a0ff8f334de54
Martin Fencl <martin.fencl@gmail.com>
2019-03-31 14:48:27 -0500

    gem letter_opener
    Preview email in the default browser instead of sending it in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,10 @@ Rails.application.configure do
   config.active_storage.service = :gcs_development
   # config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
