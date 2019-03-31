256aa04694b4efc50825e9506209b8cf30aa7d62
Martin Fencl <martin.fencl@gmail.com>
2019-03-31 11:59:37 -0500

    regenerate visitor mailer for the inquiry form
    This time with the inquiry method:
    
    $ rails g mailer VisitorMailer inquiry

--- app/mailers/visitor_mailer.rb
+++ app/mailers/visitor_mailer.rb
@@ -1,2 +1,13 @@
 class VisitorMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.visitor_mailer.inquiry.subject
+  #
+  def inquiry
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
 end
