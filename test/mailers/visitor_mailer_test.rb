2450f34d6a15cff944cff2cd21d25ac598c823aa
Martin Fencl <martin.fencl@gmail.com>
2019-03-31 12:45:14 -0500

    write failing test of VsitorMailer inquiry

--- test/mailers/visitor_mailer_test.rb
+++ test/mailers/visitor_mailer_test.rb
@@ -2,11 +2,20 @@ require 'test_helper'
 
 class VisitorMailerTest < ActionMailer::TestCase
   test "inquiry" do
-    mail = VisitorMailer.inquiry
-    assert_equal "Inquiry", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    inquiry = Inquiry.new(  name:   'anna',
+                            email:  'anna@example.org',
+                            body:   'we would like a new roof' )
+
+    mail = VisitorMailer.inquiry(inquiry)
+
+    assert_emails 1 do
+      mail.deliver_now
+    end
+
+    assert_equal "Inquiry from rhouse2.com", mail.subject
+    assert_equal ["info@rhouse2.com"], mail.to
+    assert_equal ["anna@example.com"], mail.from
+    assert_match /we would like a new roof/, mail.body.encoded
   end
 
 end
