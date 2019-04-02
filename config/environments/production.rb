072a7ad18bf2790aaeff215d4e8f5c58c78e444b
Martin Fencl <martin.fencl@gmail.com>
2019-04-01 20:13:40 -0500

    innocuous ruby syntax upfate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,12 +99,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => 'apikey',
-    :password => ENV['SENDGRID_API_KEY'],
-    :domain => 'rhouse2-staging.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    domain: 'rhouse2-staging.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
