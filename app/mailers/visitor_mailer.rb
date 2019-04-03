7730b12843c1f9a3d3fd280aa02de7af426ba5bb
Martin Fencl <martin.fencl@gmail.com>
2019-04-03 16:12:45 -0500

    use Rhouse.. insead of rhouse.. for the online inquiry recipient

--- app/mailers/visitor_mailer.rb
+++ app/mailers/visitor_mailer.rb
@@ -11,7 +11,7 @@ class VisitorMailer < ApplicationMailer
     @body = inquiry.body
 
     development_staging = Rails.env.development? || ENV['STAGING'].present?
-    domain = development_staging ? 'goodpeople.us' : 'rhouse2.com'
+    domain = development_staging ? 'goodpeople.us' : 'Rhouse2.com'
     rh2_email = "info@#{domain}"
     recipients = inquiry.cc_inquiry? ? [rh2_email, inquiry.email] : rh2_email
     from_domain = development_staging ? 'rhouse2-staging.herokuapp.com' : 'rhouse2.herokuapp.com'
