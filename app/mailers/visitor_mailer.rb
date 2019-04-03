ee2c8f2d30297ac384be5a2990e05b21baabd3e1
Martin Fencl <martin.fencl@gmail.com>
2019-04-03 15:45:56 -0500

    change inquiry from email address domain
    Remove the inquiry's email from the from email field, include it in the inquiry's body instead. This may be violating the DMARC :: SPF, DKIM email validation(s)? Still trying to make emailing work.

--- app/mailers/visitor_mailer.rb
+++ app/mailers/visitor_mailer.rb
@@ -7,15 +7,17 @@ class VisitorMailer < ApplicationMailer
   #
   def inquiry(inquiry)
     @name = inquiry.name
+    @email = inquiry.email
     @body = inquiry.body
 
     development_staging = Rails.env.development? || ENV['STAGING'].present?
     domain = development_staging ? 'goodpeople.us' : 'rhouse2.com'
     rh2_email = "info@#{domain}"
     recipients = inquiry.cc_inquiry? ? [rh2_email, inquiry.email] : rh2_email
+    from_domain = development_staging ? 'rhouse2-staging.herokuapp.com' : 'rhouse2.herokuapp.com'
 
     mail  to:       recipients,
-          from:     inquiry.email,
+          from:     "inquiry@#{from_domain}",
           subject:  'Inquiry from rhouse2.com'
   end
 end
