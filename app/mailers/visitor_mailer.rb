c43f97a5d37bd3548a5620bd3c6385556d04b4f5
Martin Fencl <martin.fencl@gmail.com>
2019-06-27 13:44:06 -0500

    change rh2 email again
    Test anew from staging.

--- app/mailers/visitor_mailer.rb
+++ app/mailers/visitor_mailer.rb
@@ -12,9 +12,9 @@ class VisitorMailer < ApplicationMailer
 
     development_staging = Rails.env.development? || ENV['STAGING'].present?
     domain = development_staging ? 'goodpeople.us' : 'rhouse2.com'
-    rh2_email = 'zx9hall@yahoo.com'
+    # rh2_email = 'zx9hall@yahoo.com'
     # rh2_email = 'rhouse2constructionllc@gmail.com'
-    # rh2_email = "info@#{domain}"
+    rh2_email = "info@#{domain}"
     recipients = inquiry.cc_inquiry? ? [rh2_email, inquiry.email] : rh2_email
     from_domain = development_staging ? 'rhouse2-staging.herokuapp.com' : 'rhouse2.herokuapp.com'
 
