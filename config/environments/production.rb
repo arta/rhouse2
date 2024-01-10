670899fac3e42cb9dbc060d84d99919c945a941a
Martin Fencl <martin@layeredcode.com>
2024-01-10 11:57:08 -0600

    Update production.rb WIP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.configure do
+Rails.application.configure do # rubocop:disable Metrics/BlockLength
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
