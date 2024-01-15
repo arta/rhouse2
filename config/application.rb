393546cc2a341d086ec829bc767d137ee276071a
Martin Fencl <martin@layeredcode.com>
2024-01-14 22:55:22 -0600

    Update config/application.rb
    2 configs had to be set here

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,27 @@ module Rhouse2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 1-14-24: from new_framework_defaults_7_0.rb (97-104):
+    # ** Please read carefully,
+    # this must be configured in config/application.rb **
+    # Change the format of the cache entry.
+    # Changing this default means that all new cache entries added to the cache
+    # will have a different format that is not supported by Rails 6.1
+    # applications.
+    # Only change this value after your application is fully deployed to
+    # Rails 7.0 and you have no plans to rollback.
+    # When you're ready to change format, add this to `config/application.rb`
+    # (NOT this file):
+    config.active_support.cache_format_version = 7.0
+
+    # 1-14-24: from new_framework_defaults_7_0.rb (141-144):
+    # ** Please read carefully, this must be configured in config/application.rb
+    # (NOT this file) **
+    # Disables the deprecated #to_s override in some Ruby core classes
+    # See https://guides.rubyonrails.org/configuring.html#
+    # config-active-support-disable-to-s-conversion
+    # for more information.
+    config.active_support.disable_to_s_conversion = true
   end
 end
